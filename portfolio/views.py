01f3e53eb3ae6d82387b146004a5a83b738d9da6
M0-o <berrami.mouad2@gmail.com>
2025-03-27 07:43:45 -0700

    added signup functionality

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render , redirect
 from django.http import HttpResponse  
 from django.contrib.auth import authenticate , login as auth_login
-
+from django.contrib.auth.models import User 
 
 def index(request):
     if request.user.is_authenticated:
@@ -21,3 +21,20 @@ def login(request):
     else:
        
         return render(request, "login.html")
+
+def signup(request):
+    if request.method == "POST":
+        firstname = request.POST.get("firstname")
+        lastname  = request.POST.get("lastname")
+        username  = request.POST.get("username")
+        email     = request.POST.get("email")
+        password  = request.POST.get("password")
+        if User.objects.filter(username=username).exists():
+            return render(request, "signup.html", {"error_message": "Username already exists"})
+        
+        user = User.objects.create_user(username=username, password=password, email=email,
+                                        first_name=firstname, last_name=lastname)
+        auth_login(request, user)
+        return redirect("index")
+    else:
+        return render(request, "signup.html")
