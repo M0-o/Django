74f931ba53eefcb155f8901ad50ac392d0b76800
M0-o <berrami.mouad2@gmail.com>
2025-03-27 07:21:46 -0700

    implemented login functionality

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render , redirect
+from django.http import HttpResponse  
+from django.contrib.auth import authenticate , login as auth_login
+
 
 def index(request):
+    if request.user.is_authenticated:
+        return HttpResponse(f"Hello,{request.user.username}.")
+    
     return HttpResponse("Hello, world. You're at the portfolio index.") 
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username=username, password=password)
+        if not user:
+            return render(request, "login.html", {"error_message": "Invalid credentials"})
+        auth_login(request, user)
+        return redirect("index")
+    else:
+       
+        return render(request, "login.html")
